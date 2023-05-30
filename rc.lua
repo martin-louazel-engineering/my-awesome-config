93354fb11927ffd05b58c710d7b3ca79804aad4b
Martin Louazel <martin.louazel@streamunlimited.com>
2023-05-30 21:00:19 +0200

    Add space and separators to wibar left layout

--- rc.lua
+++ rc.lua
@@ -245,7 +245,9 @@ awful.screen.connect_for_each_screen(function(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
+			wibox.widget.textbox("  "),
             mylauncher,
+			bar_spr,
             s.mytaglist,
             s.mypromptbox,
         },
