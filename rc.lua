5dba3557e5a8ecf616ad2739bbd1fa725934a2fe
Martin Louazel <martin.louazel@streamunlimited.com>
2023-06-05 20:52:37 +0200

    remove default Mod+Ctrl+k mapping
    Cycling through screen happens with the Mod+o mapping, and only goes in
    one direction (I will assume that I will have not more than 3 monitors
    at any given time, so that should be that much of a hindrance)

--- rc.lua
+++ rc.lua
@@ -330,8 +330,6 @@ globalkeys = gears.table.join(
               {description = "swap with previous client by index", group = "client"}),
     awful.key({ modkey              }, "o", function () awful.screen.focus_relative( 1) end,
               {description = "focus the next screen", group = "screen"}),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
-              {description = "focus the previous screen", group = "screen"}),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
               {description = "jump to urgent client", group = "client"}),
     awful.key({ modkey,           }, "Tab",
